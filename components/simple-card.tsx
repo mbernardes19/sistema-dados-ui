640757bb0cf5f4b9546932a7de175be8a7d7b881
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-21 22:30:11 -0300

    Adjusts card default size

--- components/simple-card.tsx
+++ components/simple-card.tsx
@@ -11,15 +11,15 @@ type CardProps = {
 
 export const SimpleCard: FunctionComponent<CardProps> = ({ width, height, contentFlow, contentAlign, children }) =>
     (
-        <Card style={{height: `${height}rem`, width: `${width}rem`, marginLeft: '2rem', marginRight: '2rem'}}>
-            <CardContent style={{display: 'flex', justifyContent: contentAlign, flexDirection: contentFlow, textAlign: contentAlign, paddingLeft: '2rem', paddingTop: '2rem', paddingBottom: '2rem', paddingRight: '2rem'}}>
+        <Card style={{height: `${height}rem`, width: `${width}rem`, marginLeft: '2rem', marginRight: '2rem', padding: '1rem' }}>
+            <CardContent style={{display: 'flex', justifyContent: contentAlign, flexDirection: contentFlow, textAlign: contentAlign}}>
                 { children }
             </CardContent>
         </Card>
     );
 
 SimpleCard.defaultProps = {
-    height: 26,
+    height: 24,
     width: 24,
     contentAlign: 'center',
     contentFlow: 'column'
