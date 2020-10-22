18f5265f27a201e6594ddeba708b0e375c295832
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-21 23:18:06 -0300

    Adds onClick event to button

--- components/primary-button.tsx
+++ components/primary-button.tsx
@@ -3,14 +3,16 @@ import { SimpleButton } from "./simple-button";
 
 type ButtonProps = {
     color?: string,
-    textColor?: string
+    textColor?: string,
+    onClick?: (arg?: any) => void
 }
 
-export const PrimaryButton: FunctionComponent<ButtonProps> = ({ children }) => (
+export const PrimaryButton: FunctionComponent<ButtonProps> = ({ children, onClick }) => (
     <SimpleButton
         textColor='white'
         color='#2D2D2D'
         primary
+        onClick={onClick}
     >
         { children }
     </SimpleButton>
--- components/simple-button.tsx
+++ components/simple-button.tsx
@@ -5,13 +5,15 @@ type ButtonProps = {
     color?: string,
     textColor?: string
     primary?: boolean
+    onClick?: (arg?: any) => void
 }
 
-export const SimpleButton: FunctionComponent<ButtonProps> = ({ children, color, textColor, primary }) => (
+export const SimpleButton: FunctionComponent<ButtonProps> = ({ children, color, textColor, primary, onClick }) => (
     <Button
         style={{backgroundColor: color, color: textColor}}
         variant= {primary ? 'contained' : 'text'}
         fullWidth
+        onClick={onClick}
     >
         <span style={{fontWeight: 400}}>{ children }</span>
     </Button>
