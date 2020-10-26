38be46810963c2c8453873cea0ac9d4c526678b4
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-25 22:20:59 -0300

    Connect login page to api

--- forms/login-form.tsx
+++ forms/login-form.tsx
@@ -1,9 +1,10 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { TextInput } from "../components/text-input";
 import { useForm } from 'react-hook-form';
 import styled from "styled-components";
 import { PrimaryButton } from "../components/button/primary-button";
-import { SecondaryButton } from "../components/button/secondary-button";
+import ApiService from "../services/api";
+import { useRouter } from "next/dist/client/router";
 
 type FormProps = {
 
@@ -11,19 +12,32 @@ type FormProps = {
 
 export const LoginForm: FunctionComponent<FormProps> = () => {
     const { register, handleSubmit, errors } = useForm();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
+    const router = useRouter();
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = async data => {
+        setError(false);
+        setLoading(true);
+        try {
+            const response = await ApiService.login({ email: data.email, password: data.password });
+            router.push('/menu');
+        } catch (err) {
+            setError(true);
+        }
+        setLoading(false);
+    }
 
     return (
         <Form>
             <h1 style={{color: '#66667B', marginTop:'.5rem', fontWeight: 400, marginBottom: '2rem'}}>Acesse o sistema</h1>
             <div style={{display: 'grid', gridRowGap: '1rem'}}>
+                { error ? <span style={{color: '#f44336'}}>Usuário ou senha incorretos</span> : <></> }
                 <TextInput error={errors.email ? true : false} helperText={errors.email ? "Email em formato inválido" : ""} name='email' label='Email' inputRef={register({ pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/})} />
-                <TextInput name='senha' label='Senha' type='password' inputRef={register} />
+                <TextInput name='password' label='Senha' type='password' inputRef={register} />
             </div>
             <div style={{display: 'grid', gridRowGap: '.5rem', marginTop: '2rem', marginBottom: '2rem', width: '100%'}}>
                 <PrimaryButton onClick={handleSubmit(onSubmit)}>Fazer login</PrimaryButton>
-                <SecondaryButton>Criar um cadastro</SecondaryButton>
             </div>
         </Form>
     )
