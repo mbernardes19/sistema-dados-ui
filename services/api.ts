041058bb6ef4bc9157e368128b8ecb09bf376750
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-25 22:20:08 -0300

    Adds login and register API endpoints

--- services/api.ts
+++ services/api.ts
@@ -1,14 +1,38 @@
 import HttpService from "./http";
 
+type LoginCredentials = {
+    email: string,
+    password: string
+}
+
+type RegistrationCredentials = {
+    email: string,
+    name: string,
+    enterpriseName: string,
+    isAdmin: boolean
+}
+
 export default class ApiService {
+    private static httpService: HttpService = new HttpService();
 
     static async updateSystemData(file: File) {
-        const httpService = new HttpService();
         const bodyFormData = new FormData();
         bodyFormData.append('file', file);
 
-        return await httpService.post('/data', bodyFormData, {
+        return await this.httpService.post('/data', bodyFormData, {
             headers: {'Content-Type': 'multipart/form-data'}
+        })
+    }
+
+    static async login(loginCredentials: LoginCredentials) {
+        return await this.httpService.post('/auth/login', loginCredentials, {
+            'Content-Type': 'application/json'
+        })
+    }
+
+    static async register(registrationCredentials: RegistrationCredentials) {
+        return await this.httpService.post('/register', registrationCredentials, {
+            'Content-Type': 'application/json'
         })
     }
 
