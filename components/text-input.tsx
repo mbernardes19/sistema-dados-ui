34d138e2a39e793716f61b45ba77e500371c0705
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-27 22:18:07 -0300

    Updates TextInput component props

--- components/text-input.tsx
+++ components/text-input.tsx
@@ -1,14 +1,14 @@
-import TextField, { BaseTextFieldProps } from '@material-ui/core/TextField';
+import TextField, { BaseTextFieldProps, TextFieldProps, OutlinedTextFieldProps } from '@material-ui/core/TextField';
 import { FunctionComponent } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 
-type TextInputProps = {
-    inputRef?: (instance?: any) => void
-    label: string,
-    name?: string,
-    error?: boolean,
-    helperText?: string,
-    type?: string
+interface TextInputProps extends BaseTextFieldProps {
+    inputRef?: (instance?: any) => void;
+    label: string;
+    name?: string;
+    error?: boolean;
+    helperText?: string;
+    type?: string;
 }
 
 const useStyles = makeStyles({
