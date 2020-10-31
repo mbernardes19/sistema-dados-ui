83402e69e3d5fc8f5dd4778dc81e226e49df8468
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-31 19:58:28 -0300

    Creates Order Filter

---
+++ components/order-filter.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent, ChangeEvent } from "react";
+import { SimpleCard } from "./simple-card";
+import { Dropdown } from "./dropdown";
+import Enterprise from "../model/enterprise";
+
+
+type OrderFilterProps = {
+    enterprises?: Enterprise[]
+    selected?: Enterprise
+    onChange?: (event: React.ChangeEvent<{value: Enterprise}>) => void
+}
+
+export const OrderFilter: FunctionComponent<OrderFilterProps> = ({ selected, onChange, enterprises }) => {
+    return (
+        <SimpleCard
+            height={18}
+            contentAlign='left'
+            contentFlow='row'
+        >
+            <Dropdown value={selected} onChange={onChange} getDataLabel={(data) => data.name} data={enterprises} label='Empresa' />
+        </SimpleCard>
+    )
+}
